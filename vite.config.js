290cf022fcadb9e54c62dcddf140f8dd871e4e4b
张伟叠 <862375698@qq.com>
2021-12-27 11:08:15 +0800

    fix:更改配置

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+/*
+ * @Author: zhang_weidie
+ * @LastEditors: zhang_weidie
+ * @Date: 2021-04-27 13:57:54
+ * @Description:
+ */
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  base: "./",
+});
